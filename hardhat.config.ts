ed2f2b3cf89bdaa12fc28bab00244609c150ff05
gitigs <72256997+igorsenych-cw@users.noreply.github.com>
2022-08-15 12:47:55 +0300

    Configure development environment

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    },
+    substrate: {
+      url: "http://127.0.0.1:9933",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+      gas: "auto"
+    },
+  },
+};
+
+export default config;
